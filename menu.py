afc8e984aee803cfec2d90de58ec2d065a6a9e28
Deimie <evanjoelsanchez@gmail.com>
2022-03-15 14:28:54 -0700

    Message

---
+++ menu.py
@@ -0,0 +1,15 @@
+mainMenu = [
+    ['Swap', 'Keypad', 'Christmas Tree']
+]
+
+border = '=' * 25
+banner = f"\n{border}\nPlease Select An Option\n{border}"
+
+
+def menu():
+    title = f'Function Menu{banner}'
+
+def buildmenu():
+    print()
+
+menu()
---
+++ swap.py
@@ -0,0 +1,7 @@
+age1 = input('Input first number: ')
+age2 = input('Input second number: ')
+
+myList = [age1, age2]
+myList.sort()
+
+print(myList[0] + ', ' + myList[1])
